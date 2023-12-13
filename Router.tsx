dd848149c45e8c19be14677ef9b0b23dc190c8e2
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2023-12-13 22:31:33 +0900

    Add files via upload

---
+++ Router.tsx
@@ -0,0 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { SignIn } from "../pages/SignIn";
+import { SignUp } from "../pages/SignUp";
+import { NewBreadPage } from "../pages/NewBreadPage";
+import { EditBreadPage } from "../pages/EditBreadPage";
+import { BreadReview } from "../pages/BreadReview";
+import { NotFound } from "../pages/NotFound";
+import { UserProfile } from "../pages/UserProfile";
+import { NewBreadReview } from "../pages/NewBreadReview";
+import { UserReviewList } from "../pages/UserReviewList";
+import { EditBreadReview } from "../pages/EditBreadReview";
+
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/myapp" element={<Home />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/newbreadpage" element={<NewBreadPage />} />
+      <Route path=":breadId/editbreadpage" element={<EditBreadPage />} />
+      <Route path=":breadId/newbreadreview" element={<NewBreadReview />} />
+      <Route path=":breadId/:reviewId/editbreadreview" element={<EditBreadReview />} />
+      <Route path=":breadId" element={<BreadReview />} />
+      <Route path="/users/:userId" element={<UserReviewList />} />
+      <Route path="/userprofile" element={<UserProfile />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
